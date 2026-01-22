154a23f1e6d5c7b8617b7c4d1a46b89512ddf67e
sree durga <degreeveda@gmail.com>
2026-01-22 10:03:24 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Welcome</h2>
+      <h3>To my college</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
